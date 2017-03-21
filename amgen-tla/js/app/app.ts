9323dec35d3486ee9be4b4639a524ea4c5ce7487
mike-ward <mike@mike-ward.net>
2017-03-21 11:15:26 -0400

    move dialog styles to dialog.ts

--- amgen-tla/js/app/app.ts
+++ amgen-tla/js/app/app.ts
@@ -13,55 +13,23 @@ module App {
   class Styles {
     // language=CSS
     css = `
-      body{background-color:#fff;color:#333;line-height:1.6;margin:0;margin:1em auto;max-width:1200px;min-width:320px;}
-      view{margin:1em auto;}
-      .error-text{color:darkred;}
-      .current-user-login{text-align:right;}
-
-      /* override vex button styles to look like pure-css buttons */
-      .vex.vex-theme-plain .vex-dialog-button.vex-dialog-button-primary{
-        display: inline-block;
-        zoom: 1;
-        line-height: normal;
-        white-space: nowrap;
-        vertical-align: middle;
-        text-align: center;
-        cursor: pointer;
-        -webkit-user-drag: none;
-        -webkit-user-select: none;
-           -moz-user-select: none;
-            -ms-user-select: none;
-                user-select: none;
-        box-sizing: border-box;
-        font-family: inherit;
-        padding: 0.5em 1em;
-        color: #444; 
-        color: rgba(0, 0, 0, 0.80); 
-        border: 1px solid #999; 
-        border: none rgba(0, 0, 0, 0); 
-        background-color: #E6E6E6;
-        text-decoration: none;
-        border-radius: 2px;
+      body{
+        background-color:#fff;
+        color:#333;
+        line-height:1.6;
+        margin:0;
+        margin:1em auto;
+        max-width:1200px;
+        min-width:320px;
       }
-      .vex.vex-theme-plain .vex-dialog-button:focus {
-        animation: none;
-        -webkit-animation: none;
-        -moz-animation: none;
-        -ms-animation: none;
-        -o-animation: none;
-        -webkit-backface-visibility: hidden;
-        outline: none;
-        filter: alpha(opacity=90);
-        background-image: -webkit-linear-gradient(transparent, rgba(0,0,0, 0.05) 40%, rgba(0,0,0, 0.10));
-        background-image: linear-gradient(transparent, rgba(0,0,0, 0.05) 40%, rgba(0,0,0, 0.10));
+      view{
+        margin:1em auto;
       }
-
-      /* fixes vex closing flashing in IE */
-      .vex.vex-closing .vex-content {
-        opacity: 0; 
+      .error-text{
+        color:darkred;
       }
-      .vex.vex-closing .vex-overlay {
-        opacity: 0;
+      .current-user-login{
+        text-align:right;
       }`
   }
 
--- amgen-tla/js/app/services/Dialog.ts
+++ amgen-tla/js/app/services/Dialog.ts
@@ -133,15 +133,55 @@ module App.Services {
     static getAll() {
       return vex.getAll();
     }
+  }
 
+  export const dialogStyles = {
     // language=CSS
-    css = `
+    css: `
+      /* override vex button styles to look like pure.css style buttons */
       .vex.vex-theme-plain .vex-dialog-button.vex-dialog-button-primary{
+        display: inline-block;
+        zoom: 1;
+        line-height: normal;
+        white-space: nowrap;
+        vertical-align: middle;
+        text-align: center;
+        cursor: pointer;
+        -webkit-user-drag: none;
+        -webkit-user-select: none;
+           -moz-user-select: none;
+            -ms-user-select: none;
+                user-select: none;
+        box-sizing: border-box;
+        font-family: inherit;
+        padding: 0.5em 1em;
+        color: #444; 
+        color: rgba(0, 0, 0, 0.80); 
+        border: 1px solid #999; 
+        border: none rgba(0, 0, 0, 0); 
+        background-color: #E6E6E6;
+        text-decoration: none;
+        border-radius: 2px;
+      }
+      .vex.vex-theme-plain .vex-dialog-button:focus {
+        animation: none;
+        -webkit-animation: none;
+        -moz-animation: none;
+        -ms-animation: none;
+        -o-animation: none;
+        -webkit-backface-visibility: hidden;
+        outline: none;
         filter: alpha(opacity=90);
-        color: inherit;
-        background: inherit;
-        background-image: -webkit-linear-gradient(transparent,rgba(0,0,0,.05) 40%,rgba(0,0,0,.1));
-        background-image: linear-gradient(transparent,rgba(0,0,0,.05) 40%,rgba(0,0,0,.1));
-      }`;
+        background-image: -webkit-linear-gradient(transparent, rgba(0,0,0, 0.05) 40%, rgba(0,0,0, 0.10));
+        background-image: linear-gradient(transparent, rgba(0,0,0, 0.05) 40%, rgba(0,0,0, 0.10));
+      }
+
+      /* fixes vex closing flashing in IE */
+      .vex.vex-closing .vex-content {
+        opacity: 0; 
+      }
+      .vex.vex-closing .vex-overlay {
+        opacity: 0;
+      }`
   }
 }
