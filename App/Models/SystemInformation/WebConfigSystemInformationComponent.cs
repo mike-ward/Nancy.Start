2b604821786f81f5d88cfbfb6a064fa3c401a762
mike-ward <mike@mike-ward.net>
2017-03-31 13:59:37 -0400

    add web config to system information dashboard

---
+++ App/Models/SystemInformation/WebConfigSystemInformationComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Nancy;
+using Nancy.Helpers;
+
+namespace App.Models.SystemInformation
+{
+    public class WebConfigSystemInformationComponent : ISystemInformationComponent
+    {
+        private readonly IRootPathProvider _rootPathProvider;
+
+        public WebConfigSystemInformationComponent(IRootPathProvider rootPathProvider)
+        {
+            _rootPathProvider = rootPathProvider;
+        }
+
+        public int Cardinality => 3;
+
+        public string Title()
+        {
+            return "Web.config";
+        }
+
+        public string Html()
+        {
+            return $"<pre>{GetData()}</pre>";
+        }
+
+        private string GetData()
+        {
+            var config = File.ReadAllText(_rootPathProvider.GetRootPath() + "Web.config");
+            return HttpUtility.HtmlEncode(config);
+        }
+    }
+}
