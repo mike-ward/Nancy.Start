0972e1822b53fdff8581ce7720ee8218e6833b02
mike-ward <mike@mike-ward.net>
2017-03-31 14:49:36 -0400

    add app settings and connection strings

---
+++ App/Models/SystemInformation/AppSettingsSystemInformationComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+
+namespace App.Models.SystemInformation
+{
+    public class AppSettingsSystemInformationComponent : ISystemInformationComponent
+    {
+        public int Cardinality => 5;
+
+        public string Title()
+        {
+            return "App Settings";
+        }
+
+        public string Html()
+        {
+            return $"<pre>{GetData()}</pre>";
+        }
+
+        private string GetData()
+        {
+            var appSettings = string.Empty;
+
+            foreach (var key in  ConfigurationManager.AppSettings.AllKeys)
+                appSettings += $"{key,-25}{ConfigurationManager.AppSettings[key]}{Environment.NewLine}";
+
+            return appSettings;
+        }
+    }
+}
---
+++ App/Models/SystemInformation/ConnectionStringsSystemInformationComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using Nancy.Helpers;
+
+namespace App.Models.SystemInformation
+{
+    public class ConnectionStringsSystemInformationComponent : ISystemInformationComponent
+    {
+        public int Cardinality => 4;
+
+        public string Title()
+        {
+            return "Connection Strings";
+        }
+
+        public string Html()
+        {
+            return $"<pre>{GetData()}</pre>";
+        }
+
+        private string GetData()
+        {
+            var connectionStrings = string.Empty;
+
+            foreach (ConnectionStringSettings cs in  ConfigurationManager.ConnectionStrings)
+                connectionStrings += $"{cs.Name,-25}{cs.ConnectionString}{Environment.NewLine}";
+
+            return HttpUtility.HtmlEncode(connectionStrings);
+        }
+    }
+}
